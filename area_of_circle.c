67546b7f06f5738100abc4be3a22e06be71e10a5
Santanu Samanta <124898558+SANTANU9999@users.noreply.github.com>
2024-02-05 21:47:35 +0530

    Add files via upload

---
+++ area_of_circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+float radius;
+printf("Ente the Circle readius ");
+scanf("%f", &radius);
+printf("So Area of Circle Is = ");
+printf("%f", 3.14 * radius * radius);
+return 0;
+}
---
+++ data_type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int age = 22;
+    float pi = 3.14;
+    char percentage = '%';
+    printf("age is %d\n", age);
+    printf("parsentage is is %f\n", pi);
+    printf("age is %c", percentage);
+    return 0;
+}
